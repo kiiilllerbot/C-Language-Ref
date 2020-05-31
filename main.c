eebe78158b29af4d9bce82f3c03a2939d05e61ed
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-01 00:14:16 +0800

    Intro To C Language

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
+// Introduction To C Language
+
+/*
+C Language: C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system. By design, C provides constructs that map efficiently to typical machine instructions.
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     printf("Hello, World!\n");
