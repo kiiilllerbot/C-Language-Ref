3eadb611730671f0fd23b92fe4cffe64e9f5d4b9
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-01 00:09:31 +0800

    Template Setup

---
+++ cmake-build-debug/CMakeFiles/testC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testC.dir/main.c.obj"
+  "libtestC.dll.a"
+  "testC.exe"
+  "testC.exe.manifest"
+  "testC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/testC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for testC.
+# This may be replaced when dependencies are built.
