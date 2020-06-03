ad587a4c408277984dd78726f9ccda5f0e39c11a
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-03 15:03:39 +0800

    C Program Structure

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
+/* C Program Structure
+    A C program involves the following sections:
+        * Documentations (Documentation Section)
+        * Preprocessor Statements (Link Section)
+        * Global Declarations (Definition Section)
+        * The main() function
+        * Local Declarations
+        * Program Statements & Expressions
+        * User Defined Functions
+
+        All C programs must have a main() which contains two parts:
+            * Declaration part
+            * Execution part
+ */
+
+// Sample Program
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World!\n"); // Hello, World!
     return 0;
 }
