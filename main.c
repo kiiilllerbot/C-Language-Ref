d4069d26b813509c90136b4842d46671cb5c021d
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-03 19:21:12 +0800

    C Else-If Statements

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
-#include <stdio.h>
+/* C If-Else Statements
+    else-if statements in C is like another if condition, it's used in a program when if statement having multiple decisions.
 
+    Syntax:
+    if(test_expression)
+    {
+       //execute your code
+    }
+    else if(test_expression n)
+    {
+       //execute your code
+    }
+    else
+    {
+       //execute your code
+    }
+
+*/
+
+// Sample Program
+#include <stdio.h>
 int main() {
-    printf("Hello, World!\n");
+    int a,b,c;
+
+    printf("Enter A Number For a: ");
+    scanf("%d", &a);
+
+    printf("Enter A Number For b: ");
+    scanf("%d", &b);
+
+    printf("Enter A Number For c: ");
+    scanf("%d", &c);
+
+    if ( a > b && a > c ){
+        printf("A is greater than b, and c\n");
+    }else if(b > a && b > c){
+        printf("B is greater than a, and c\n");
+    }else{
+        printf("C is greater than a, and b\n");
+    }
+
     return 0;
 }
