cdf95ac63a6c4d96e2626756712ec067e43843ac
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-03 18:27:28 +0800

    C Format Specifiers

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+/* C Format Specifiers
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+    In C programming we need lots of format specifier to work with various data types. Format specifiers defines the type of data to be printed on standard output. Whether to      print formatted output or to take formatted input we need format specifiers. Format specifiers are also called as format string.
+
+    %c -> Character -> char, unsigned char
+    %d -> Signed integer -> short, unsigned short, int, long
+    %e or %E -> Scientific notation of float values -> float, double
+    %f -> Floating point -> float
+    %g or %G -> Similar as %e or %E -> float, double
+    %hi -> signed integer (short) -> short
+    %hu -> unsigned integer (short) -> unsigned short
+    %i -> Signed integer -> short, unsigned short, int, long
+    %l or %ld or %li -> Signed int -> long
+    %lf -> Floating point -> double
+    %Lf -> Floating point -> long double
+    %lu -> Unsigned int -> unsigned int, unsigned long
+    %lli, %lld -> Signed int -> long long
+    %llu -> unsigned int -> unsigned long long
+    %o -> Octal representation of Integer -> short, unsigned short, int, unsigned int, long
+    %p -> Address of pointer to void void * -> void *
+    %s -> String -> char *
+    %u -> Unsigned int -> Hexadecimal representation of Unsigned Integer -> short, unsigned short, int, unsigned int, long
+    %n -> Prints nothing
+    %% -> Prints % character
+
+ */
