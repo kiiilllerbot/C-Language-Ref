f3f5f1a622f96ba1f20911f25b41260e78df9fad
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-03 19:13:26 +0800

    C Nested If Else Statements

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
+/* C Nested If-Else Statements
+
+    Nested if else statements play an important role in C programming, it means you can use conditional statements inside another conditional statement.
+    Syntax:
+    if(test_expression one)
+    {
+       if(test_expression two) {
+          //Statement block Executes when the boolean test expression two is true.
+       }
+    }
+    else
+    {
+        //else statement block
+    }
+
+*/
+
+// Sample Program
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char userName[] = "shimol-khan";
+    bool loggedIn = true;
+
+    if (loggedIn){
+        if (userName){
+            printf("Welcome! %s\n", userName); // Welcome! shimol-khan
+        }
+    }else{
+        printf("Plz Sign In To The Console First\n"); // Will not be executed
+    }
     return 0;
 }
