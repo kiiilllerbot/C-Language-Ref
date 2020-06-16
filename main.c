0cb993c7c00c875a364350aa4b15ed4abb11925b
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-17 00:46:10 +0800

    C Do While Loop

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
-#include <stdio.h>
+/* C Do While Loop
+ * C do while loops are very similar to the while loops, but it always executes the code block at least once and furthermore as long as the condition remains true. This is an          exit-controlled loop.
 
-int main() {
-    printf("Hello, World!\n");
+   Syntax:
+   do{
+       statement(s);
+    }while( condition );
+*/
+
+// Sample Program
+#include<stdio.h>
+int main (){
+    int n = 1,times=5;
+    do
+    {
+        printf("C do while loops: %d\n", n);
+        n = n + 1;
+    }while( n <= times );
     return 0;
 }
+
+/* Output
+
+C do while loops: 1
+C do while loops: 2
+C do while loops: 3
+C do while loops: 4
+C do while loops: 5
+ */
