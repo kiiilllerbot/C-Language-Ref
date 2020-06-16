52f7f13128e09c9af28076c815690cbd0768d6cd
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-17 00:50:30 +0800

    C For Loop

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-#include <stdio.h>
+/* C For Loop
+ * C for loops is very similar to a while loops in that it continues to process a block of code until a statement becomes false, and everything is defined in a single line. The for loop is also entry-controlled loop.
 
-int main() {
-    printf("Hello, World!\n");
+  Syntax:
+  for ( init; condition; increment ){
+       statement(s);
+    }
+
+ */
+
+// Sample Program
+#include<stdio.h>
+int main (){
+    int n,times=5;;
+    for( n = 1; n <= times; n = n + 1 )
+    {
+        printf("C for loops: %d\n", n);
+    }
     return 0;
 }
+
+/* Output
+  C for loops: 1
+  C for loops: 2
+  C for loops: 3
+  C for loops: 4
+  C for loops: 5
+ */
