14509235d6e4087d197e55875f5bffe566633d95
Shimol-Khan <se.shimolkhan@gmail.com>
2020-06-18 01:53:24 +0800

    C Strings

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+/* C Strings
+   In C programming, the one-dimensional array of characters are called strings, which is terminated by a null character '\0'.
 
-int main() {
-    printf("Hello, World!\n");
+   -> String declaration C through array of characters: char name[6];
+   -> String declaration C through pointer: char *name;
+   -> String initialization in C: char name[6] = {'C', 'l', 'o', 'u', 'd', '\0'}; 0r char name[] = "Cloud";
+*/
+
+// Sample Program
+#include<stdio.h>
+int main (){
+    char name[6] = {'C', 'l', 'o', 'u', 'd', '\0'};
+
+    printf("Tutorials%s\n", name ); //=> TutorialsCloud
+
     return 0;
 }
